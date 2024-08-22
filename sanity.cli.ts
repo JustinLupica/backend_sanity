77a64bd026273242babf4da96e41705f3835adbb
Justin Lupica <jll689@ftr.com>
2024-08-22 08:29:26 -0400

    feat: bootstrap sanity studio

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'i9c69yb5',
+    dataset: 'production'
+  }
+})
---
+++ sanity.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {structureTool} from 'sanity/structure'
+import {schemaTypes} from './schemaTypes'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'Justin Lupica\'s Portfolio',
+
+  projectId: 'i9c69yb5',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
